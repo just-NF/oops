f95036c9aef81fca7a225da05947900c1a18ba18
just-NF <ajokedi.aane@gmail.com>
2025-04-30 21:01:06 +0100

    creating classes and loops

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public static void main(String [] args){
     Student student1 = new Student();
     Student student2 = new Student();
     //set value
-    student1.name = "spongebob";
+    student1.name = "fish";
     student1.age = 10;
 
     System.out.println(student1.name);
